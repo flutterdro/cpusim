1d6508bc4c10da1713b7526aa27d5632652f949b
aa <aaa>
2023-05-06 16:34:03 +0300

    Initial Commit

---
+++ cpusim/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpusim
+//
+//  Created by flutterdro on 06.05.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
